1ca2343b3bf4ebe33c3f07e5f00df8192cad46db
amitpp <amitpp23@gmail.com>
2014-12-03 16:20:26 +0530

    :

---
+++ prabhas.c
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int 
+count <<"Enter number of elements in array";
+cin>>n
+for(i=0;i<n;i++)
+{
+cout<<"Enter an element";
+cin>>A[i];
+}
+
+for(i=0;i<n;i++)
+{
+ count=0;
+ for(j=0;j<=A[i];j++)
+  {
+    count++;
+  }
+ cout<<count/3;
+} 
+
+}
